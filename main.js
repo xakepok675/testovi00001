e8ab83e1f6b37255948142e59e4af16956f66a1b
xakepok675 <xakepok7771@gmail.com>
2026-03-03 20:17:28 +0300

    commit-3

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const gap = 6;
-
 const carousel = document.getElementById("carousel"),
-  content = document.getElementById("content"),
-  next = document.getElementById("next"),
-  prev = document.getElementById("prev");
+content = document.getElementById("content"),
+next = document.getElementById("next"),
+prev = document.getElementById("prev");
 
 next.addEventListener("click", e => {
   carousel.scrollBy(width + gap, 0);
@@ -14,6 +13,7 @@ next.addEventListener("click", e => {
     next.style.display = "none";
   }
 });
+
 prev.addEventListener("click", e => {
   carousel.scrollBy(-(width + gap), 0);
   if (carousel.scrollLeft - width - gap <= 0) {
@@ -25,4 +25,44 @@ prev.addEventListener("click", e => {
 });
 
 let width = carousel.offsetWidth;
+
 window.addEventListener("resize", e => (width = carousel.offsetWidth));
+
+const section = document.querySelector('.rpdsss');
+const nextSection = document.querySelector('.hero');
+const nav__scroll = document.querySelector('.nav__scroll'); 
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      nav__scroll.classList.add('active');
+    } 
+    else {
+      if (entry.boundingClientRect.top > 0) {
+        nav__scroll.classList.remove('active');
+      }
+    }
+  });
+}, {
+  threshold: 0.8
+});
+observer.observe(section);
+
+const nextObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      nav__scroll.classList.remove('active');
+    }
+  });
+}, {
+  threshold: 0.3
+});
+nextObserver.observe(nextSection);
+
+window.addEventListener('load', () => {
+  const rect = section.getBoundingClientRect();
+  const triggerPoint = window.innerHeight * 0.5;
+  if (rect.top < triggerPoint) {
+    nav__scroll.classList.add('active');
+  }
+});
